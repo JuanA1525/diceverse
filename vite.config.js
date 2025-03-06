213bdde32f55d87b77dd9945e4fb5f388a436166
Juan Porras <juana.porras@upb.edu.co>
2025-03-05 20:24:56 -0500

    fix: Se repara deploy con ghpages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Diceverse/',  // Añade esta línea
+  base: '/diceverse/',  // Añade esta línea
 })
